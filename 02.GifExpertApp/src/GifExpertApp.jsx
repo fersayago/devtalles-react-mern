ea772b82bfb666691ab5f6bd74c368fc5eedac0c
fersayago <sayago.fernando@gmail.com>
2022-10-18 15:13:43 -0300

    Agregar una nueva categoria

--- 02.GifExpertApp/src/GifExpertApp.jsx
+++ 02.GifExpertApp/src/GifExpertApp.jsx
@@ -4,11 +4,16 @@ const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['Ciudad de Dios', 'Batman']);
 
+  const onAddCategory = (category) => {
+    setCategories(state => [ ...state, category])
+  }
+
   return (
     <>
       {/* Titulos */}
       <h1>GifExpertApp</h1>
       {/* Input */}
+      <button onClick={() => {onAddCategory("valorant")}}>add</button>
       {/* Listado de Gif */}
       <ol>
         {
