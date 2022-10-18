d6710f92d0f0706f8d630aa6ba64fda2db621e05
fersayago <sayago.fernando@gmail.com>
2022-10-18 13:54:44 -0300

    Inicio del proyecto - GifExpertApp

---
+++ 02.GifExpertApp/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GifExpertApp from './GifExpertApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>
+)
+
+// APIKEY=0yozM6lsSlv4u6HCwUqaLP4GJBbWPLF1
