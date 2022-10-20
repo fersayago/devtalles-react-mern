d97daace974b8e1a56b7dbfb64d417d0e2661d5e
fersayago <sayago.fernando@gmail.com>
2022-10-20 16:04:39 -0300

    Mostrar los titulos de las imagenes

--- 02.GifExpertApp/src/components/GifGrid.jsx
+++ 02.GifExpertApp/src/components/GifGrid.jsx
@@ -1,15 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import fetchGifs from '../api/fetchGifs'
 
 const GifGrid = ({ category }) => {
 
+  const [images, setImages] = useState([])
+
+  // ! useEffect no puede devolver una promesa
   useEffect(() => {
     fetchGifs(category)
+      .then(
+        newImages => setImages(newImages)
+      )
   },[])
 
   return (
     <>
       <h3>{ category }</h3>
+
+      <ul>
+        {
+          images.map(({ id, title }) => (
+            <li key={id}>
+              <p>{ title }</p>
+            </li>
+          ))
+        }
+      </ul>
     </>
   )
 }
