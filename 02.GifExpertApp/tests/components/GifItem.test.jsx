90f1b5b75796672c8502c9f1f945a09e9bbaf397
fersayago <sayago.fernando@gmail.com>
2022-10-24 17:18:10 -0300

    test GifItem

--- 02.GifExpertApp/tests/components/GifItem.test.jsx
+++ 02.GifExpertApp/tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import GifItem from '../../src/components/GifItem'
 
 describe('Pruebas en <GifItem />', () => {
@@ -13,5 +13,26 @@ describe('Pruebas en <GifItem />', () => {
     )
 
     expect( container ).toMatchSnapshot()
+  })
+
+  test('debe mostrar la imagen con el URL y el ALT indicado', () => {
+    render(
+      <GifItem title={ title } url={ url } /> 
+    )
+
+    const renderedImage = screen.getByRole('img');
+
+    expect( renderedImage.src ).toBe(url);
+    expect( renderedImage.alt ).toBe(title);
+  })
+
+  test('debe mostrar el titulo indicado en el componente', () => {
+    render(
+      <GifItem title={ title } url={ url } /> 
+    )
+
+    expect( screen.getByText( title )).toBeTruthy();
+
+
   })
 })
