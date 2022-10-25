452cd84227776e43014fd3f7282882c1744fe60e
fersayago <sayago.fernando@gmail.com>
2022-10-25 20:11:23 -0300

    Jest Functions

--- 02.GifExpertApp/tests/components/addCategory.test.jsx
+++ 02.GifExpertApp/tests/components/addCategory.test.jsx
@@ -3,8 +3,6 @@ import AddCategory from "../../src/components/AddCategory";
 
 describe ('Pruebas en <AddCategory />', () => {
 
-  const newCategory = 'Dragon Ball';
-
   test('debe de cambiar el valor de la caja de texto', () => {
 
     // tenemos que mandar una función
@@ -21,15 +19,33 @@ describe ('Pruebas en <AddCategory />', () => {
   test('debe de llamar onNewCategory si el input tiene un valor', () => {
 
     const inputValue = 'Saitama';
+    const onNewCategory = jest.fn(); // jest function: simula función
 
-    render( <AddCategory onNewCategory={ () => {} } /> )
+    render( <AddCategory onNewCategory={ onNewCategory } /> )
     
     const input = screen.getByRole('textbox');
     const form = screen.getByRole('form'); // lo busca mediante el aria-label
 
-    fireEvent.input( input, { target: { value: 'Saitama' } })
+    fireEvent.input( input, { target: { value: inputValue } })
     fireEvent.submit( form )
     
     expect( input.value ).toBe('');
+
+    expect( onNewCategory ).toHaveBeenCalledTimes(1);
+
+    expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+  })
+
+  test('no debe llamar el onNewCategory si el input esta vacío', () => {
+
+    const onNewCategory = jest.fn();
+
+    render( <AddCategory onNewCategory={ onNewCategory } /> )
+
+    const form = screen.getByRole('form');
+    fireEvent.submit( form );
+
+    // expect( onNewCategory ).toHaveBeenCalledTimes(0);
+    expect( onNewCategory ).not.toHaveBeenCalled();
   })
 })
