d7ef98a0a013322d69cfcddb6ccc65a123988b36
fersayago <sayago.fernando@gmail.com>
2022-10-25 20:30:02 -0300

    Mock a CustomHook

--- 02.GifExpertApp/tests/components/GifGrid.test.jsx
+++ 02.GifExpertApp/tests/components/GifGrid.test.jsx
@@ -1,11 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import GifGrid from "../../src/components/GifGrid";
+import useFetchGifs from "./../../src/hooks/useFetchGifs"
+
+// no solo sirve para hacer mock de mi libreria sino también de librerias de terceros
+jest.mock('./../../src/hooks/useFetchGifs')
 
 describe('Pruebas en <GifGrid />', () => {
 
   const testCategory = 'jest'
 
   test('debe de mostrar el loading inicialmente', () => {
+
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading:  true
+    })
 
     render( <GifGrid  category={ testCategory }/> );
 
@@ -14,7 +23,34 @@ describe('Pruebas en <GifGrid />', () => {
   })
 
   test('debe de mostrar items cuando se cargan las imagenes mediante useFetchGifs', () => {
-    
+
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'Saitama',
+        url: 'https://localhost/saitama.jpg'
+      },
+      {
+        id: 'ABC',
+        title: 'Goku',
+        url: 'https://localhost/Goku.jpg'
+      },
+      {
+        id: 'ABC',
+        title: 'Luffy',
+        url: 'https://localhost/Luffy.jpg'
+      }
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    })
+
+  render( <GifGrid category={ testCategory } />)
+
+  expect( screen.getAllByRole('img').length ).toBe(3);
+
   })
 
 })
