36eb5f56a542eec9afc16a94356af82d8928fd7d
fersayago <sayago.fernando@gmail.com>
2022-10-26 15:16:15 -0300

    useState

---
+++ 03.hook-app/src/01-useState/CounterApp.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+
+const CounterApp = () => {
+
+  const [ counters, setCounters ] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+
+  const { counter1, counter2, counter3 } = counters;
+
+  
+
+  return (
+    <>
+      <h1>Counter1: { counter1 }</h1>
+      <h1>Counter2: { counter2 }</h1>
+      <h1>Counter3: { counter3 }</h1>
+
+      <hr />
+
+      <button className='btn' onClick={
+        () => setCounters({ ...counters, counter1: counter1+1 })
+      }>+1</button>
+    </>
+  )
+}
+
+export default CounterApp
--- 03.hook-app/src/HooksApp.jsx
+++ 03.hook-app/src/HooksApp.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import CounterApp from './01-useState/CounterApp'
 
 const HooksApp = () => {
   return (
+    <>
     <h1>HooksApp</h1>
+
+    <CounterApp />
+    </>
   )
 }
 
