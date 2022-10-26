3f54cb72aa523d1cf1f3ed42fcda62fbcf7aa98d
fersayago <sayago.fernando@gmail.com>
2022-10-26 11:36:46 -0300

    custom hooks tests

---
+++ 02.GifExpertApp/tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,34 @@
+import { renderHook, waitFor } from "@testing-library/react"
+import useFetchGifs from "./../../src/hooks/useFetchGifs"
+
+
+describe('Pruebas en hook useFetchGifs', () => {
+
+  test('debe de regresar el estado inicial', () => {
+
+    const { result } = renderHook(() => useFetchGifs('Dragon Ball'));
+
+    const { images, isLoading } = result.current;
+
+    expect( images.length ).toBe(0);
+    expect( isLoading ).toBe(true);
+  })
+
+  test('debe de retornar un arreglo de imagenes y el isLoading false', async() => {
+
+    const { result } = renderHook(() => useFetchGifs('Dragon Ball'));
+    
+    // espera a que las imagenes se reciban
+    await waitFor(
+      () => expect( result.current.images. length ).toBeGreaterThan(0),
+      {
+        timeout: 3000
+      }
+    );
+
+    const { images, isLoading } = result.current;
+
+    expect( images.length ).toBeGreaterThan(0);
+    expect( isLoading ).toBeFalsy();
+  })
+})
