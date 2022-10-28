d3790dc2b20b5bf89f4d1810235e7f560ba9339c
fersayago <sayago.fernando@gmail.com>
2022-10-27 21:12:11 -0300

    Formulario con custom hooks

---
+++ 03.hook-app/src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,61 @@
+import useForm from '../hooks/useForm';
+import Message from './Message';
+
+const FormWithCustomHook = () => {
+
+  const {
+    username,
+    email,
+    password,
+    formState,
+    onInputChange
+  } = useForm({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  // para evitar esta desestructuración estamos devolviendo los campos individualmente en el hook useForm
+  //const { username, email, password } = formState
+
+  return (
+    <>
+      <h1>Formulario con custom hook</h1>
+      <hr />
+
+      <input
+        type="text"
+        className='form-control mt-2'
+        placeholder='Usuario'
+        name='username'
+        value={username}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="email"
+        className='form-control mt-2'
+        placeholder='Correo'
+        name='email'
+        value={email}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="password"
+        className='form-control mt-2'
+        placeholder='Contraseña'
+        name='password'
+        value={password}
+        onChange={onInputChange}
+      />
+
+      {
+        username === 'usuarioExistente' &&
+        <Message />
+      }
+    </>
+  )
+}
+
+export default FormWithCustomHook
