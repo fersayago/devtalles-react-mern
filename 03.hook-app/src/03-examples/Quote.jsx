054478a972c6c9033e3e321a6bd123ff9bc65645
fersayago <sayago.fernando@gmail.com>
2022-10-30 19:45:06 -0300

    useLayoutEffect

--- 03.hook-app/src/03-examples/Quote.jsx
+++ 03.hook-app/src/03-examples/Quote.jsx
@@ -1,10 +1,33 @@
+import { useLayoutEffect, useRef, useState } from "react"
 
 const Quote = ({ quote, author }) => {
+
+  const pRef = useRef();
+  const [boxSize, setBoxSize] = useState({
+    width: 0,
+    height: 0
+  })
+
+  // se dispara cuando el componente elegido se renderiza completamente
+  useLayoutEffect(() => {
+    const { width, height } = pRef.current.getBoundingClientRect()
+
+    setBoxSize({width, height})
+    console.log(boxSize)
+  }, [quote])
+
   return (
-    <blockquote className='blockquote text-end'>
-      <p className='mb-1'>{ quote }</p>
-      <footer className='blockquote-footer'>{ author }</footer>
-    </blockquote>
+    <>
+      <blockquote
+        className='blockquote text-end'
+        style={{ display: 'flex' }}
+        >
+        <p ref={ pRef } className='mb-1'>{ quote }</p>
+        <footer className='blockquote-footer'>{ author }</footer>
+      </blockquote>
+
+      <code>{ JSON.stringify(boxSize) }</code>
+    </>
   )
 }
 
---
+++ 03.hook-app/src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,56 @@
+import { useFetch, useCounter } from '../hooks'
+import { QuoteLoader, Quote } from './../03-examples'
+
+const Layout = () => {
+
+  const {
+    counter,
+    incrementar,
+    decrementar,
+    reiniciar,
+  } = useCounter(1);
+
+  const {data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${ counter }`)
+
+  const { author, quote } = !!data && data[0]
+
+  return (
+    <>
+      <h1>Breaking Bad Quotes</h1>
+      <hr />
+
+      {
+        isLoading
+          ? <QuoteLoader />
+          : <Quote quote={ quote } author={ author } />
+      }
+      
+      <button
+        disabled={ isLoading || counter <= 1 }
+        className='btn btn-primary'
+        onClick={ () => decrementar() } // si invocamos incrementar vamos a enviar el evento click mientras que la funcion espera un valor
+      >
+        Previous quote
+      </button>
+
+      <button
+        disabled={ isLoading }
+        className='btn btn-primary'
+        onClick={ () => incrementar() } // si invocamos incrementar vamos a enviar el evento click mientras que la funcion espera un valor
+      >
+        Next quote
+      </button>
+
+      <button
+        disabled={ isLoading || counter === 1 }
+        className='btn btn-primary'
+        onClick={ () => reiniciar() } // si invocamos incrementar vamos a enviar el evento click mientras que la funcion espera un valor
+      >
+        First Quote
+      </button>
+
+    </>
+  )
+}
+
+export default Layout
