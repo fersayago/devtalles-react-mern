33ef245817235f08d24bb160d17599a93bfa2c1f
fersayago <sayago.fernando@gmail.com>
2022-11-14 12:13:46 -0300

    Leer argumentos por URL

--- 05.heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 05.heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,20 @@
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers"
 
 const HeroPage = () => {
+
+  const { heroId } = useParams()
+
+  const hero = getHeroById( heroId );
+
+  if ( !hero ){
+    return <Navigate to='/marvel' />
+  }
+
   return (
-    <h1>HeroPage</h1>
+    <>
+      <h1>{hero.superhero}</h1>
+    </>
   )
 }
 
