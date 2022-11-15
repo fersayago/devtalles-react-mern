c732d8891d3b7915af0d184a6569b66c51f91733
fersayago <sayago.fernando@gmail.com>
2022-11-15 10:50:19 -0300

    Mantener el usuario activo

--- 05.heroes-spa/src/auth/context/AuthProvider.jsx
+++ 05.heroes-spa/src/auth/context/AuthProvider.jsx
@@ -3,22 +3,34 @@ import { types } from "../types/types"
 import { AuthContext } from "./"
 import { authReducer } from "./"
 
-const initialState = {
-  logged: false,
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') );
+
+  // si el usuario existe logged en true
+  return {
+    logged: !!user,
+    user: user
+  }
 }
 
 const AuthProvider = ({ children }) => {
   
-  const [ authState, dispatch ] = useReducer( authReducer, initialState)
+  // enviamos un objeto vacio ya que la función de inicialización sera la que indique el usuario y su estado
+  const [ authState, dispatch ] = useReducer( authReducer, {}, init)
 
   const handleLogin = ( name = '' ) => {
+
+    const user = {
+      id: 'ABC',
+      name
+    }
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name
-      }
+      payload: user
     }
+
+    localStorage.setItem( 'user', JSON.stringify(user) );
 
     dispatch( action )
   }
