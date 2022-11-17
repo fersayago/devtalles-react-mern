ce2c6125c70f75fd9f82f02b3370a2ca2a5d78d5
fersayago <sayago.fernando@gmail.com>
2022-11-17 17:10:26 -0300

    JournalLayout y JournalPage

---
+++ 06.journal-app/src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/system"
+
+const drawerWidth = 240;
+
+const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex '}}>
+      {/* Navbar drawerWidth */}
+
+      {/* Sidebar drawerWidth */}
+
+      <Box
+        component='main'
+        sx={{ flexGrow: 1, p: 3 }}
+      >
+        {/* Toolbar */}
+        { children }
+      </Box>
+    </Box>
+  )
+}
+
+export default JournalLayout
--- 06.journal-app/src/journal/pages/JournalPage.jsx
+++ 06.journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Typography } from '@mui/material'
+import JournalLayout from '../layout/JournalLayout'
 
 const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error quod aliquid cum quasi quaerat accusantium. Adipisci repellat quo architecto, labore quia qui, quibusdam distinctio saepe, eum ab reiciendis consequuntur eligendi?</Typography>
+    
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
   )
 }
 
