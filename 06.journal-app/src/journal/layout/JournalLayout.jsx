184520600f1655a3a9b40bf4c27ec8769087c9f6
fersayago <sayago.fernando@gmail.com>
2022-11-17 17:27:50 -0300

    NavBar

--- 06.journal-app/src/journal/layout/JournalLayout.jsx
+++ 06.journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,12 @@
 import { Box } from "@mui/system"
+import { NavBar } from "../components";
 
 const drawerWidth = 240;
 
 const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex '}}>
-      {/* Navbar drawerWidth */}
+      <NavBar drawerWidth={ drawerWidth }/>
 
       {/* Sidebar drawerWidth */}
 
