13bdc9a3d02180082ad64358d027c96315b4195c
fersayago <sayago.fernando@gmail.com>
2022-11-30 01:47:29 -0300

    Pruebas con useContext

--- 03.hook-app/src/09-useContext/HomePage.jsx
+++ 03.hook-app/src/09-useContext/HomePage.jsx
@@ -3,11 +3,15 @@ import { UserContext } from "./context/UserContext"
 
 const HomePage = () => {
 
-  const { hola,  user } = useContext( UserContext )
+  const { hola, user } = useContext( UserContext )
   return (
     <>
       <div>HomePage <small>{ user?.name && user.name}</small></div>
       <p>{ hola }</p>
+
+      <pre aria-label='pre'>
+        { JSON.stringify( user, null, 3 ) }
+      </pre>
     </>
   )
 }
