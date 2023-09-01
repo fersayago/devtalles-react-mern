bff4597dce93c98c3619947eaa85dae7d7293011
fersayago <sayago.fernando@gmail.com>
2023-09-01 12:30:06 -0300

    S18: Axios

--- 07.toolkit-redux/src/store/slices/pokemon/thunks.js
+++ 07.toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from '../../../api/pokemonApi';
 import { startLoadingPokemons, setPokemons } from './pokemonSlice';
 
 export const getPokemons = ( page = 0) => {
@@ -5,7 +6,13 @@ export const getPokemons = ( page = 0) => {
     dispatch( startLoadingPokemons() ) 
 
     // TODO: realizar petición http
+    // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10}`)
+    // const data = await resp.json()
+    const { data } = await pokemonApi.get(`pokemon?limit=10&offset=${ page * 10}`)
 
-    // dispatch( setPokemons() )
+    dispatch(setPokemons({
+      pokemons: data.results,
+      page: page + 1
+    }))
   }
 }
