9f0a713b581be6d5440dea49b72bc1781f895992
fersayago <sayago.fernando@gmail.com>
2023-09-01 19:03:07 -0300

    S18: RTK Query ejemplo

--- 07.toolkit-redux/src/main.jsx
+++ 07.toolkit-redux/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { store } from './store'
 import { Provider } from 'react-redux';
-import App from './App'
-import PokemonApp from './PokemonApp';
+// import App from './App'
+// import PokemonApp from './PokemonApp';
+import { TodoApp } from './TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PokemonApp />
+      {/* <App />
+      <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 )
