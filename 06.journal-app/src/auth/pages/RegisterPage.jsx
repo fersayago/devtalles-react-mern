ae92cf3e90937eecd2034de9056287be31cf6bc4
fersayago <sayago.fernando@gmail.com>
2023-09-04 12:02:40 -0300

    S19: Manejo de errores del formulario

--- 06.journal-app/src/auth/pages/RegisterPage.jsx
+++ 06.journal-app/src/auth/pages/RegisterPage.jsx
@@ -10,9 +10,24 @@ const formData = {
   displayName: 'Fernando Sayago'
 }
 
+const formValidations = {
+  email: [(value) => value.includes('@'), 'El correo debe tener un @.'],
+  password: [(value) => value.length >= 6, 'El password debe de tener mas de 6 .'],
+  displayName: [(value) => value.length >= 1, 'El nombre es obligatorio.']
+}
+
 const RegisterPage = () => {
 
-  const { displayName, email, password, onInputChange, formState } = useForm(formData)
+  const {
+    displayName,
+    email, password,
+    onInputChange,
+    formState,
+    isFormValid,
+    displayNameValid,
+    passwordValid,
+    emailValid,
+  } = useForm(formData, formValidations)
 
   const onSubmit = (event) => {
     event.preventDefault()
@@ -34,6 +49,8 @@ const RegisterPage = () => {
               name='displayName'
               value={ displayName }
               onChange={ onInputChange }
+              error={ !displayNameValid }
+              helperText={ displayNameValid }
             />
           </Grid>
           <Grid item xs={ 12 } sx={{ mt: 2}}>
--- 06.journal-app/src/hooks/useForm.js
+++ 06.journal-app/src/hooks/useForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
-  
+export const useForm = ( initialForm = {}, formValidations = {} ) => {
+
     const [ formState, setFormState ] = useState( initialForm );
 
     const onInputChange = ({ target }) => {
